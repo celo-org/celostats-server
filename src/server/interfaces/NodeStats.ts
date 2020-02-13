4d14f95d71b56a7780281aa40202cf50f9c08200
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-13 11:08:23 +0100

    format

--- src/server/interfaces/NodeStats.ts
+++ src/server/interfaces/NodeStats.ts
@@ -2,7 +2,7 @@ import { StatsSummary } from "./StatsSummary"
 import { Address } from "./Address"
 
 export interface NodeStats {
-  readonly id: Address,
+  readonly id: Address
   readonly name: string
   readonly stats: StatsSummary
   readonly history: number[]
