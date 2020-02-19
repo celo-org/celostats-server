c5e53a3d39d0b3f2f7c6030c457883f4195bbf06
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-19 08:47:22 +0100

    - add index

---
+++ src/server/interfaces/index.ts
@@ -0,0 +1,27 @@
+import { Pending } from "./Pending";
+import { NodeSummary } from "./NodeSummary";
+import { ChartData } from "./ChartData";
+import { Latency } from "./Latency";
+import { BlockStats } from "./BlockStats";
+import { NodeDetails } from "./NodeDetails";
+import { StatsResponse } from "./StatsResponse";
+import { ClientPong } from "./ClientPong";
+import { ClientPing } from "./ClientPing";
+import { LastBlock } from "./LastBlock";
+import { BlockSummary } from "./BlockSummary";
+import { Miner } from "./Miner";
+
+export {
+  Pending,
+  NodeSummary,
+  ChartData,
+  Latency,
+  BlockStats,
+  NodeDetails,
+  StatsResponse,
+  ClientPong,
+  ClientPing,
+  LastBlock,
+  BlockSummary,
+  Miner
+}
