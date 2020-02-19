b131530ccd1438f06b591a128eccff0a152a651a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-19 12:02:06 +0100

    - expose js instead of ts

---
+++ src/server/index.ts
@@ -0,0 +1,29 @@
+import { Pending } from "./interfaces/Pending";
+import { NodeSummary } from "./interfaces/NodeSummary";
+import { ChartData } from "./interfaces/ChartData";
+import { Latency } from "./interfaces/Latency";
+import { BlockStats } from "./interfaces/BlockStats";
+import { NodeDetails } from "./interfaces/NodeDetails";
+import { StatsResponse } from "./interfaces/StatsResponse";
+import { ClientPong } from "./interfaces/ClientPong";
+import { ClientPing } from "./interfaces/ClientPing";
+import { LastBlock } from "./interfaces/LastBlock";
+import { BlockSummary } from "./interfaces/BlockSummary";
+import { Miner } from "./interfaces/Miner";
+import { Events } from "./server/Events";
+
+export {
+  Pending,
+  NodeSummary,
+  ChartData,
+  Latency,
+  BlockStats,
+  NodeDetails,
+  StatsResponse,
+  ClientPong,
+  ClientPing,
+  LastBlock,
+  BlockSummary,
+  Miner,
+  Events
+}
