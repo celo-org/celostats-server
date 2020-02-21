886b32c573e092f717538025630d45617b03bc83
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-21 16:13:43 +0100

    configure web3 properly

--- src/server/ContractKit.ts
+++ src/server/ContractKit.ts
@@ -1,24 +1,27 @@
 import { cfg } from "./utils/config"
 import { ValidatorsWrapper } from "@celo/contractkit/lib/wrappers/Validators"
-import { ContractKit, newKit } from "@celo/contractkit"
+import { ContractKit, newKitFromWeb3 } from "@celo/contractkit"
 import { ElectionWrapper } from "@celo/contractkit/lib/wrappers/Election"
 import Web3 from "web3";
 
+const web3: Web3 = new Web3(new Web3.providers.HttpProvider(cfg.JSONRPC, {
+    keepAlive: true,
+    timeout: cfg.timeout
+  })
+);
+
 let validators: ValidatorsWrapper = null;
 let election: ElectionWrapper = null;
-let web3: Web3 = null;
-
 let contractsLoaded = false;
 
 (async () => {
   if (!contractsLoaded) {
     try {
-      const kit: ContractKit = await newKit(cfg.JSONRPC)
+      const kit: ContractKit = await newKitFromWeb3(web3)
 
       // load contracts
       validators = await kit.contracts.getValidators()
       election = await kit.contracts.getElection();
-      web3 = kit.web3;
 
       console.success('Contract kit loaded!')
 
