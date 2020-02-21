817bd1c331624f3f4a83db3bf23c01cec1dafa71
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-21 16:36:54 +0100

    add contracts debugger

--- src/server/server/routes.ts
+++ src/server/server/routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { cfg } from "../utils/config"
+import { getContracts } from "../ContractKit"
 
 export const routes = express.Router()
 
@@ -34,29 +36,86 @@ routes.get('/memory', (
 })
 
 /**
- * catch 404 and forward to error handler
+ * Config view
  */
-routes.use((
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  const err = new Error(`Not Found ${req.url}`)
-  res.status(404)
-  next(err)
-})
-
-/**
- * Error handler
- */
-routes.use((
-  err: any,
+routes.get('/config', (
   req: express.Request,
   res: express.Response
 ) => {
-  res.status(err.status || 500)
-  res.render('error', {
-    message: err.message,
-    error: err
-  })
+  res.set('Content-Type', 'text/html');
+  res.send(
+    Buffer.from(
+      `<pre>${JSON.stringify(cfg, null, 2)}</pre>`
+    )
+  )
 })
+
+/**
+ * Contract view
+ */
+routes.get('/contracts', (
+  req: express.Request,
+  res: express.Response
+) => {
+  const contracts = getContracts()
+
+  res.set('Content-Type', 'text/html');
+
+  if (contracts) {
+
+    (async () => {
+      const css = await contracts.validators.currentSignerSet()
+      const election = await contracts.election.getConfig()
+      res.send(Buffer.from(
+        `
+<pre>
+Node: ${cfg.JSONRPC}
+
+Current Signer Set: ${JSON.stringify(css, null, 2)}
+
+Election Config: ${JSON.stringify(election, null, 2)}
+
+Highest Block: ${await contracts.web3.eth.getBlockNumber()}
+</pre>`
+      ))
+    }
+  )()
+
+    } else {
+      res.send(
+        Buffer.from('Contracts lot loaded :(')
+      )
+    }
+  })
+
+  /**
+   * catch 404 and forward to error handler
+   */
+  routes.use((
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const err = new Error(
+
+    `Not Found ${req.url}`
+
+    )
+    res.status(404)
+    next(err)
+  })
+
+  /**
+   * Error handler
+   */
+  routes.use((
+    err: any,
+    req: express.Request,
+    res: express.Response
+  ) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
