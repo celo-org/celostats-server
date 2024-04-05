6c36c631e2dfbc92dd5353548c99a9d072bcb3e7
Javier Cortejoso <javier.cortejoso@gmail.com>
2024-04-05 10:51:08 +0000

    Revert chalk version

--- src/server/utils/logger.ts
+++ src/server/utils/logger.ts
@@ -1,18 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let chalk: any;
-
-async function loadChalk() {
-    chalk = await import('chalk');
-}
-
-loadChalk();
-
+import chalk from "chalk"
 import * as util from "util"
 
 declare global {
   interface Console {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    success: any;
+    success: any
   }
 }
 const sections = [
@@ -163,7 +155,8 @@ styles.forEach(function (item) {
     if (typeof args[0] === 'object') {
       message = util.inspect(args[0], {depth: null, colors: true})
     } else {
-      message = item.messageColor(util.format(args[0], ...args.slice(1)))
+      // eslint-disable-next-line prefer-spread
+      message = item.messageColor(util.format.apply(util, args))
     }
 
     // @ts-expect-error fix linter
